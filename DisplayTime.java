fbf2754121828f851d87f35d9219394ccef50c35
kobayashi1987 <sashagao1987@gmail.com>
2025-01-07 21:20:28 +0800

    test github 2025.01.07

---
+++ DisplayTime.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DisplayTime {
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter an integer for base number: ");
+//        int base = input.nextInt();
+//
+//        System.out.print("Enter an integer for exponent number: ");
+//        int exponent = input.nextInt();
+//
+//        String result = String.format("result is %d ^ %d = %d",base,exponent, (int)Math.pow(base, exponent));
+//        System.out.println(result);
+
+        System.out.println("the modulus of -45 % -7 is " + (-34 % -5));
+
+    }
+}
+
+// test github 2025.01.07
+
